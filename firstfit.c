4e746c8fcc439ad302a204d197494e1db2d4c565
Dev Atul Patel <114083457+devvp8@users.noreply.github.com>
2023-05-11 20:24:55 +0530

    Update firstfit.c

--- firstfit.c
+++ firstfit.c
@@ -1,48 +1,34 @@
 #include <stdio.h>
+
 void main()
 {
-    int bsize[15], psize[15], bno, pno, flags[15], allocation[15], i, j;
-    for (i = 0; i < 15; i++)
+    int n;
+    int n_mem;
+    int mem_loc[100];
+    printf("Enter no of memory partitions\n");
+    scanf("%d",&n_mem);
+    int mem_sizes[] = {200, 400, 600, 500, 300, 250};
+    int process_sizes[] = {357, 210, 468, 491};
+    printf("Enter no of processes\n");
+    scanf("%d",&n);
+    int j=0;
+    for(int i=0;i<n_mem;i++)
     {
-        flags[i] = 0;
-        allocation[i] = -1;
-    }
-    printf("enter no of Memory Blocks : ");
-    scanf("%d", &bno);
-    printf("enter size of each Memory Block : \n");
-    for (i = 0; i < bno; i++)
-        scanf("%d", &bsize[i]);
-    printf("enter no of Processes : ");
-    scanf("%d", &pno);
-    printf("enter size of each Process : \n");
-    for (i = 0; i < pno; i++)
-        scanf("%d", &psize[i]);
-    for (i = 0; i < pno; i++)
-    {
-        int indexPlaced = -1;
-        for (j = 0; j < bno; j++)
+        if(process_sizes[j] <= mem_sizes[i])
         {
-            if (bsize[j] >= psize[i] && flags[j] == 0)
-            {
-                if (indexPlaced == -1)
-                    indexPlaced = j;
-                else if (bsize[j] < bsize[indexPlaced])
-                    indexPlaced = j;
-            }
-        }
-        if (indexPlaced != -1)
-        {
-            allocation[i] = indexPlaced;
-            flags[indexPlaced] = 1;
+            printf("\nfor %d , %d  %d",j,process_sizes[j],mem_sizes[i]);
+            mem_loc[j] = j+1;
+            j++;
         }
     }
-    printf("\nProcess No.\tMemory Size\t\tBlock No.\t\tProcess Size");
-    for (i = 0; i < pno; i++)
+    printf("\nFinal\n");
+    for(int i =0;i<n;i++)
     {
-        printf("\n%d\t\t%d\t\t\t", i + 1, bsize[allocation[i]]);
-        if (allocation[i] != -1)
-            printf("%d\t\t\t%d", allocation[i] + 1, psize[i]);
-        else
-            printf("Not allocated");
+        if(mem_loc[i])
+        {
+            printf("Process size = %d goes in location %d\n",process_sizes[i],mem_loc[i]);
+        }
+        else{printf("%d not allocated to memory",process_sizes[i]);}
     }
+
 }
